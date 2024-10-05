89a60aa08c65fecb26812a6afaf13ddfc3b23946
Daniel González Briseño <qbixmex@gmail.com>
2024-10-05 16:57:46 -0700

    Local Authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import type { MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
 
 const privateRoutes = [
@@ -6,19 +7,31 @@ const privateRoutes = [
 
 export const onRequest = defineMiddleware((context, next) => {
 
-  const authHeaders = context.request.headers.get('authorization');
+  const authHeaders = context.request.headers.get('authorization') ?? '';
 
   if (privateRoutes.includes(context.url.pathname)) {
-    if (authHeaders) {
-      return next();
-    }
-    return new Response('Forbidden Page', {
-      status: 401,
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"',
-      }
-    });
+    return checkAuth(authHeaders, next);
   }
 
   return next();
 });
+
+const checkAuth = (authHeaders: string, next: MiddlewareNext) => {
+  if (!authHeaders) {
+    const authValue = authHeaders.split(' ').at(-1) ?? 'user:pass';
+    const decodedValue = atob(authValue).split(':');
+    const [ user, password ] = decodedValue;
+    
+    if (user === "admin" && password === "admin") {
+      return next();
+    }
+  }
+
+  return new Response('Forbidden Page', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+      charset: "UTF-8"
+    }
+  });
+};
