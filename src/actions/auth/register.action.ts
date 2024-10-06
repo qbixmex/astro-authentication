5f4057de8c15e85663e553c46f709d7cdaf6db4c
Daniel González Briseño <qbixmex@gmail.com>
2024-10-06 06:01:31 -0700

    Register form validation

--- src/actions/auth/register.action.ts
+++ src/actions/auth/register.action.ts
@@ -1,20 +1,26 @@
-import { defineAction } from "astro:actions";
+import { defineAction, ActionError } from "astro:actions";
 import { z } from "astro:content";
 
 const registerUser = defineAction({
   accept: "form",
   input: z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-    password_confirmation: z.string().min(6),
-    remember_me: z.boolean().optional(),
+    name: z.string({ message: "Name is required !" }),
+    email: z
+      .string({ message: "Email is required !" })
+      .email({ message: "Email format is invalid !" }),
+    password: z
+      .string({ message: "Password is required !" })
+      .min(6, { message: "Password must be at least 6 characters long !" }),
+    password_confirmation: z
+      .string({ message: "Password Confirmation is required !" })
+      .min(6, { message: "Password Confirmation must be at least 6 characters long !" }),
+    remember_me: z.boolean({ message: "Remember me must be a boolean value" }).optional(),
   }).refine(data => data.password === data.password_confirmation, {
     message: "Passwords don't match !",
     path: ["password_confirmation"],
   }),
   handler: async (input, context) => {
-
+    //* Cookies
     if (input.email) {
       context.cookies.set("email", input.email, {
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365), //? 1 year
@@ -24,10 +30,22 @@ const registerUser = defineAction({
       context.cookies.delete("email", { path: "/" });
     }
 
-    return {
-      ok: true,
-      message: "User registered successfully 👍",
-    };
+    if (!context.cookies.has('user-session')) {
+      throw new ActionError({
+        code: "UNAUTHORIZED",
+        message: "User must be logged in.",
+      });
+    }
+    
+    try {
+      return {
+        ok: true,
+        message: "User registered successfully 👍",
+      };
+    } catch (error) {
+      console.error(error);
+      throw new Error('Something went wrong 😢, check server logs ⚠️');
+    }
   },
 });
 
